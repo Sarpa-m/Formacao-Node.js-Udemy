bebc4173c407c5d91c4b33c745a47725adfe6b6b
Maurício Sarpa <mauricio@dev>
2021-10-06 19:15:20 -0300

    fi

--- Projeto-02/database/database.js
+++ Projeto-02/database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 var connection = new Sequelize('p2', 'userP2', 'userP2userP2', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-03:00"
 });
 
 module.exports = connection;
