c3d681c5af71d1ca5446db0d6dc988593392a118
Maurício Sarpa <mausarpa02@gmai.com>
2022-04-19 14:31:31 -0300

    recomeço

--- Projeto-02/database/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-var connection = new Sequelize('p2', 'userP2', 'userP2userP2', {
-    host: 'localhost',
-    dialect: 'mysql',
-    timezone: "-03:00"
-});
-
-module.exports = connection;
